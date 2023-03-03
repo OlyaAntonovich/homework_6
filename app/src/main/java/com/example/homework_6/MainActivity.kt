d72784e0320be18448a82c6e44d429143ccfd83d
Olya Antonovich <ynonaj@gmail.com>
2023-03-03 21:54:51 +0300

    Start making fragment_first, import some dependencies

--- app/src/main/java/com/example/homework_6/MainActivity.kt
+++ app/src/main/java/com/example/homework_6/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.homework_6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.container,FragmentFirst())
+            .commit()
     }
 }
